e2d6cff013842a1e45c1daeaa110c36f1dac1bb6
Niklas Ekman <niklas@niklasekman.com>
2023-08-15 17:47:09 +0300

    Adding a ruby ingredient item

---
+++ src/main/java/com/niklasekman/mccourse/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.niklasekman.mccourse.item;
+
+import com.niklasekman.mccourse.MCCourseMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        MCCourseMod.LOGGER.info("Registering Mod Items for " + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
