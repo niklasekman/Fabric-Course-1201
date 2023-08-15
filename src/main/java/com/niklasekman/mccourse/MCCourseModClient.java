555475ed31013f2565eb21ae3574437b661b5315
Niklas Ekman <niklas@niklasekman.com>
2023-08-15 17:07:36 +0300

    Initial commit

---
+++ src/main/java/com/niklasekman/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package com.niklasekman.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
